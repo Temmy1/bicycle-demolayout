cd3d6d792850fcd88c2b0260e112dcfefbc7fd77
Artom Ryzhkov <temmyriz@gmail.com>
2019-06-07 16:53:00 +0300

    last changes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
-(function() {
+$(function () {
+
+  // Scroll Effects
+
+  // $(window).scroll(function() {
+
+  //   let wScroll = $(this).scrollTop();
+    
+  //   if(wScroll <= $('.header__nav').height()) {
+
+  //     $('.header__nav').css({
+  //       'transform' : 'translate(0px, '+ wScroll / .8 +'%)'
+  //     });
+  //   }
+  // });
+
+
+
+
+
+  // Carousel 
   $('.carousel-wrapper').slick({
     dots: true,
     arrows: false,
     // autoplay: true,
     autoplaySpeed: 5000
   });
-}());
+
+});
+  
