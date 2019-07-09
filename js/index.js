8458eb05fbadbefbd8b3a92a7eb9622109ccd8e5
Artom Ryzhkov <temmyriz@gmail.com>
2019-07-09 16:44:52 +0300

    added media queries

--- js/index.js
+++ js/index.js
@@ -1,28 +1,10 @@
 $(function () {
 
-  // Scroll Effects
-
-  // $(window).scroll(function() {
-
-  //   let wScroll = $(this).scrollTop();
-    
-  //   if(wScroll <= $('.header__nav').height()) {
-
-  //     $('.header__nav').css({
-  //       'transform' : 'translate(0px, '+ wScroll / .8 +'%)'
-  //     });
-  //   }
-  // });
-
-
-
-
-
   // Carousel 
   $('.carousel-wrapper').slick({
     dots: true,
     arrows: false,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 5000
   });
 
